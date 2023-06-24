05f07a29f292bb7b3c34759b0d4391cec64e7c25
Caio Martins Mendes Rodrigues <caio03martins@gmail.com>
2023-06-24 17:05:19 -0300

    Add: local storage

--- content.js
+++ content.js
@@ -1,12 +1,23 @@
 
 document.addEventListener('DOMContentLoaded', function () {
-    var colorInput = document.getElementById("colorInput");
-    var copyButton = document.getElementById("copyButton");
-    var element = document.getElementById("bolinha");
-    copyButton.addEventListener('click', function () {
-        var selectedColor = colorInput.value;
+    let colorInput = document.getElementById("colorInput");
+    let copyButton = document.getElementById("copyButton");
+    let element = document.getElementById("bolinha");
+    let colorHex = document.getElementById('colorHex')
+    let colorStorage = localStorage.getItem('color') || '#000000'
+    colorInput.value = colorStorage
+    element.style.backgroundColor = `${colorInput.value}`;
+    colorHex.innerHTML = `Color hex: ${colorInput.value}`
 
+    colorInput.addEventListener('input', function () {
+        let selectedColor = colorInput.value;
+        localStorage.setItem('color', selectedColor)
+        colorHex.innerHTML = `Color hex: ${colorInput.value}`
         element.style.backgroundColor = `${colorInput.value}`;
+    })
+
+    copyButton.addEventListener('click', function () {
+        let selectedColor = colorInput.value;
         navigator.clipboard.writeText(selectedColor)
             .then(function () {
                 console.log("Cor copiada: " + selectedColor);
